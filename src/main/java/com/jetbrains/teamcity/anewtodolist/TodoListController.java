5dae1400f3691b9395dd595fd275dcf53d3b97ed
Marco Behler <marco.behler@jetbrains.com>
2023-04-13 13:05:48 +0200

    java 11 compatibility

--- src/main/java/com/jetbrains/teamcity/anewtodolist/TodoListController.java
+++ src/main/java/com/jetbrains/teamcity/anewtodolist/TodoListController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 import java.util.concurrent.CopyOnWriteArrayList;
 
 @RestController
@@ -15,8 +16,45 @@ public class TodoListController {
 
     public List<Todo> todos = new CopyOnWriteArrayList<>(Arrays.asList(new Todo("never done", false)));
 
-    public record Todo(String name, Boolean done) {
-    }
+    public static final class Todo {
+        private final String name;
+        private final Boolean done;
+
+        public Todo(String name, Boolean done) {
+            this.name = name;
+            this.done = done;
+        }
+
+        public String name() {
+            return name;
+        }
+
+        public Boolean done() {
+            return done;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (obj == this) return true;
+            if (obj == null || obj.getClass() != this.getClass()) return false;
+            var that = (Todo) obj;
+            return Objects.equals(this.name, that.name) &&
+                    Objects.equals(this.done, that.done);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(name, done);
+        }
+
+        @Override
+        public String toString() {
+            return "Todo[" +
+                    "name=" + name + ", " +
+                    "done=" + done + ']';
+        }
+
+        }
 
 
     @GetMapping("/todos")
--- src/main/java/com/jetbrains/teamcity/anewtodolist/UserController.java
+++ src/main/java/com/jetbrains/teamcity/anewtodolist/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 import java.util.concurrent.CopyOnWriteArrayList;
 
 @RestController
@@ -13,8 +14,37 @@ public class UserController {
 
     public List<User> users = new CopyOnWriteArrayList<>(Arrays.asList(new User("Marco")));
 
-    public record User(String nam) {
-    }
+    public static final class User {
+        private final String nam;
+
+        public User(String nam) {
+            this.nam = nam;
+        }
+
+        public String nam() {
+            return nam;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (obj == this) return true;
+            if (obj == null || obj.getClass() != this.getClass()) return false;
+            var that = (User) obj;
+            return Objects.equals(this.nam, that.nam);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(nam);
+        }
+
+        @Override
+        public String toString() {
+            return "User[" +
+                    "nam=" + nam + ']';
+        }
+
+        }
 
 
     @GetMapping("/users")
